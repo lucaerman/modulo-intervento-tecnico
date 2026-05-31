6b1c16b7782c2aff1f57151a5b0936fe3eb3a7d6
lucaerman <erman.luca.98@hotmail.com>
2026-05-31 20:41:55 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rapporto-tecnico-v27';
+const CACHE_NAME = 'rapporto-tecnico-v28';
 const ASSETS_TO_CACHE = [
   './',
   './index.html', // Cambialo con il nome esatto del tuo file HTML se differente
